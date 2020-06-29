82376acf38d05dcc17c2a78a898ff5b835dcca2b
Matej Rojec <matej.4001@gmail.com>
2020-06-29 19:14:31 +0200

    dodal vektorski produkt,  množenje vektorja s skalarjem ter primerjavo vektorjev

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class Vektorji:
         if self.stevilovrstic == other.stevilovrstic:
             nov_vektor = []
             for i in range(self.stevilovrstic):
-                nov_vektor.append(self.vektor[i] + other.vektor[j])
+                nov_vektor.append(self.vektor[i] + other.vektor[i])
         else:
             raise Exception("Vektorja imata različno število vrstic, zatorej se ju ne da zmnožiti")
     
@@ -29,6 +29,18 @@ class Vektorji:
             norma += i ** 2
         return norma
     
+    def __lt__(self, other):
+        if self.stevilovrstic == other.stevilovrstic:
+            for i in self.vektor:
+                seznam = []
+                if self.vektor[i] > other.vektor[i]:
+                    seznam.append(True)
+                else:
+                    seznam.append(False)
+            return all(seznam)
+        else:
+            raise Exception("Vektorjev različnih velikosti ne moremo primerjati.")
+
     def skalarni_produkt(self, other):
     # Govorimo o standardem skalarnem produktu v R^n po komponentah
         if self.stevilovrstic == other.stevilovrstic:
@@ -37,28 +49,43 @@ class Vektorji:
                 produkt += self.vektor[i] * other.vektor[i]
             return produkt
         else:
-            raise Exception("Vektorja imata različno število vrstic, zatorej se ju ne da")
+            raise Exception("Vektorja imata različno število vrstic, zatorej se ju ne da.")
      
+    def mnozenje_vektorja_s_skalarjem(self, skalar):
+        novi = []
+        for i in range(self.stevilovrstic):
+            novi.append(skalar * self.vektor[i])
+        return novi
+    
     def vektorski_produkt(self, other):
-        pass
+        if self.stevilovrstic == 3 and other.stevilovrstic == 3:
+            s = self.vektor
+            o = other.vektor
+            novi = [None, None, None]
+            novi[0] = s[1] * o[2] - o[1] * s[2]
+            novi[1] = - s[0] * o[2] + o[0] * s[2]
+            novi[2] = s[0] * o[1] - o[0] * s[1]
+            return novi
+        else: 
+            raise Exception("Vektorski produkt je definiran le v treh dimenzijah.")
 
     def kot(self, other):
-        pass
+        return
     
     def ploščina_paralelograma(self, other):
         pass
 
-    def ploščina_paralelepipeda(self, other, drugi):
+    def ploščina_paralelepipeda(self, other, tretji):
         pass
     
-    def volumen_piramide(self, other, drugi):
+    def volumen_piramide(self, other, tretji):
         pass
 
     def enacba_premice(self, tocka):
     # enačba premice s smernim vekotrem self, ki gre skozi točko ''tocka''
         pass
 
-    def razdalja_tocke_do_premice(self, tocka1, tocka2  ):
+    def razdalja_tocke_do_premice(self, tocka1, tocka2 ):
     # self je smerni vektor premice, tocka1 je tocka ki je na premice, tocka2 pa tocka, katere razdalja do premice nas zanima
         pass
 
@@ -79,7 +106,7 @@ class Matrika:
                     pomozna.append(self.matrika[i][j] + other.matrika[i][j])
                 summation.append(pomozna)
         else:
-            raise Exception("Matriki se ne da zmnožiti, saj sta sešteti velikosti")
+            raise Exception("Matriki se ne da zmnožiti, saj sta sešteti velikosti.")
     
     def __repr__(self):
         return f'Matrika({self.matrika})'
@@ -97,7 +124,7 @@ class Matrika:
                 trace += self.matrika[i][i]
             return trace
         else:
-            raise Exception("Sled je definirana le za kvadratne matrike")
+            raise Exception("Sled je definirana le za kvadratne matrike.")
 
     def produkt(self, other):
         pass
