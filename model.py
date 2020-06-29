2c2f33f0b751ce2594e94dd99925eab8e5e56b30
Matej Rojec <matej.4001@gmail.com>
2020-06-29 17:32:28 +0200

    dodal transponiranje, sled in identiteto ter malo preuredil obliko

--- model.py
+++ model.py
@@ -1,34 +1,5 @@
 ''' Začetek z delom na kalkulatorju '''
 
-class Matrika:
-
-    def __init__(self, matrika):
-        self.matrika = matrika
-        self.stevilovrstic = len(matrika)
-        self.stevilostolpcev = len(matrika[0])
-
-    def __add__(self, other):
-        if self.stevilostolpcev == other.stevilostolpcev and self.stevilovrstic == other.stevilovrstic:
-            summation = []
-            for i in range(self.stevilovrstic):
-                pomozna = []
-                for j in range(self.stevilostolpcev):
-                    pomozna.append(self.matrika[i][j] + other.matrika[i][j])
-                summation.append(pomozna)
-        else:
-            raise Exception("Matriki se ne da zmnožiti, saj sta sešteti velikosti")
-     def __repr__(self):
-        return f'Matrika({self.matrika})'
-
-    def __sub__(self, other):
-        return self.matrika + (-1) * other.matrika
-    
-    def __eq__(self, other):
-        return self.matrika == other.matrika
-    
-    def produkt(self, other):
-
-
 class Vektorji:
 
     def __init__(self, vektor):
@@ -41,7 +12,7 @@ class Vektorji:
             for i in range(self.stevilovrstic):
                 nov_vektor.append(self.vektor[i] + other.vektor[j])
         else:
-            raise Exception("Vektorja imata različno število vrstic, zatorej se ju ne da")
+            raise Exception("Vektorja imata različno število vrstic, zatorej se ju ne da zmnožiti")
     
     def __repr__(self):
         return f'Vektor({self.vektor})'
@@ -68,10 +39,10 @@ class Vektorji:
         else:
             raise Exception("Vektorja imata različno število vrstic, zatorej se ju ne da")
      
-     def vektorski_produkt(self, other):
+    def vektorski_produkt(self, other):
         pass
 
-     def kot(self, other):
+    def kot(self, other):
         pass
     
     def ploščina_paralelograma(self, other):
@@ -84,8 +55,70 @@ class Vektorji:
         pass
 
     def enacba_premice(self, tocka):
+    # enačba premice s smernim vekotrem self, ki gre skozi točko ''tocka''
         pass
 
     def razdalja_tocke_do_premice(self, tocka1, tocka2  ):
     # self je smerni vektor premice, tocka1 je tocka ki je na premice, tocka2 pa tocka, katere razdalja do premice nas zanima
         pass
+
+class Matrika:
+
+    def __init__(self, matrika):
+        self.matrika = matrika
+        self.stevilovrstic = len(matrika)
+        self.stevilostolpcev = len(matrika[0])
+        self.kvadratna = (self.stevilovrstic == self.stevilovrstic)
+
+    def __add__(self, other):
+        if self.stevilostolpcev == other.stevilostolpcev and self.stevilovrstic == other.stevilovrstic:
+            summation = []
+            for i in range(self.stevilovrstic):
+                pomozna = []
+                for j in range(self.stevilostolpcev):
+                    pomozna.append(self.matrika[i][j] + other.matrika[i][j])
+                summation.append(pomozna)
+        else:
+            raise Exception("Matriki se ne da zmnožiti, saj sta sešteti velikosti")
+    
+    def __repr__(self):
+        return f'Matrika({self.matrika})'
+
+    def __sub__(self, other):
+        return self.matrika + (-1) * other.matrika
+    
+    def __eq__(self, other):
+        return self.matrika == other.matrika
+
+    def trace(self):
+        if self.kvadratna:
+            trace = 0
+            for i in self.stevilovrstic:
+                trace += self.matrika[i][i]
+            return trace
+        else:
+            raise Exception("Sled je definirana le za kvadratne matrike")
+
+    def produkt(self, other):
+        pass
+
+    def transponiranje(self):
+        transponiranka = []
+        for i in range(self.stevilostolpcev):
+            vrstica = []
+            for j in range(self.stevilovrstic):
+                vrstica.append(self.matrika[j][i])
+            transponiranka.append(vrstica)
+        return transponiranka
+
+def identiteta(n):
+    matrika = []
+    for i in range(n):
+        vrstica = []
+        for j in range(n):
+            if i == j:
+                vrstica.append(1)
+            else:
+                vrstica.append(0)
+        matrika.append(vrstica)
+    return matrika
